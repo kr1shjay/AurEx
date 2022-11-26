04b8870b69489c84cbc5d899f336722db33f40cf
Mat-helper <admin@maticz.com>
2022-11-26 15:36:54 +0530

    Update emailGateway.js

--- aurex_backend/lib/emailGateway.js
+++ aurex_backend/lib/emailGateway.js
@@ -15,45 +15,42 @@ let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
 
-export const sendEmail = async (to, content) => {
-    try {
+// export const sendEmail = async (to, content) => {
+   // try {
+       // let Redis = await get('SMTP')
+     //   let Type =  JSON.parse(Redis)
+       //     const { subject, template } = content;
+       //     sendSmtpEmail.subject = subject;
+       //     sendSmtpEmail.htmlContent = template;
+       //     sendSmtpEmail.sender = { "name": SendinBlue.name, email: SendinBlue.email };
+       //     sendSmtpEmail.to = [{ "email": to, "name": to }];
+       //     sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
+       //     apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+       //         console.log('Message sent: %s' + JSON.stringify(data));
+       //     }, function (error) {
+       //         return console.log(error);
+       //     });
+  //  }
+  //  catch (err) {
+  //  }
+//}
 
-        let Redis = await get('SMTP')
-        let Type =  JSON.parse(Redis)
-        // if (Type.mailType == 'sendinBlue') {
-            const { subject, template } = content;
-            sendSmtpEmail.subject = subject;
-            sendSmtpEmail.htmlContent = template;
-            sendSmtpEmail.sender = { "name": SendinBlue.name, email: SendinBlue.email };
-            sendSmtpEmail.to = [{ "email": to, "name": to }];
-            sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
-            apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
-                console.log('Message sent: %s' + JSON.stringify(data));
-            }, function (error) {
-                return console.log(error);
-            });
-
-        // } else if (Type.mailType == 'nodeMailer') {
-        //     const { subject, template } = content;
-        //     let transporter = nodemailer.createTransport(smtpConfig.nodemailer);
-        //     let info = await transporter.sendMail({
-        //         from: smtpConfig.fromMail,
-        //         to,
-        //         subject,
-        //         html: template
-        //     });
-        //     console.log("Message sent: %s", info.messageId);
-        // }
-
-    }
-    catch (err) {
-    }
+export const sendMail = async (to, content) => {
+    const { subject, template } = content;
+SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = SendinBlue.apiKey;
+// SendinBlue.apiKey
+new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
+  {
+    'subject': subject,
+    'sender' : {'email':SendinBlue.email, 'name': SendinBlue.name},
+    'replyTo' : {'email': SendinBlue.email, 'name':SendinBlue.name},
+    'to' : [{'name': to, 'email': to}],
+    'htmlContent' : template,
+    'params' : {'bodyMessage':'subject'}
+  }
+).then(function(data) {
+  console.log("New Mail sent :",data);
+}, function(error) {
+  console.error(error);
+});
 }
-
-// sendEmail(
-//     "ajith@britisheducationonline.org",
-//     {
-//         subject: 'test',
-//         template: '<h1>Test</h1>',
-//     }
-// )
