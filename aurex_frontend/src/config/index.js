a9b26b9ff7064b10e9cc9d0acb604fe8a5efccce
Mat-helper <admin@maticz.com>
2022-12-02 19:49:16 +0530

    Update index.js

--- aurex_frontend/src/config/index.js
+++ aurex_frontend/src/config/index.js
@@ -4,13 +4,13 @@ if (process.env.REACT_APP_MODE === "production") {
     console.log("Set Production Config")
     const API_URL = 'https://api.cluxchange.com';
     key = {
-        secretOrKey: "dZ@fD#%WqPQcGSa34QYF1W72uac7jYLk",
+        secretOrKey: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
         CRYPTO_SECRET_KEY: "1234567812345678",
-        RECAPTCHA_SITE_KEY: "6LeKwCYjAAAAAMbORefOGhr-1AAXaTNTkL7MBJrV", //local
-        API_URL: 'https://api.aurexchange.com/',
-        FRONT_URL: 'https://aurexchange.com/',
-        ADMIN_URL: 'https://controls.aurexchange.com',
-        SOCKET_URL: 'https://api.aurexchange.com/',
+        RECAPTCHA_SITE_KEY: "6LfCM7kgAAAAAK8DAXuDJsqm3alreybJ2TKpPaU3", //local
+        API_URL: 'https://api.cluxchange.com/',
+        FRONT_URL: 'https://cluxchange.com/',
+        ADMIN_URL: 'https://cluxchange.com/admin',
+        SOCKET_URL: 'https://api.cluxchange.com/',
         getGeoInfo: "https://ipapi.co/json/",
         AUTHENTICATOR_URL: {
             PLAY_STORE: "https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2",
@@ -18,6 +18,43 @@ if (process.env.REACT_APP_MODE === "production") {
         }
     };
 
+}else if (process.env.REACT_APP_MODE === "demo") {
+    console.log("Set Demo Config")
+    const API_URL = 'https://cluxapi.wealwin.com';
+
+    key = {
+        secretOrKey: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
+        CRYPTO_SECRET_KEY: "1234567812345678",
+        RECAPTCHA_SITE_KEY: "6LfBAlcgAAAAAIeo-0Q2m2gh35MnNt7GDfbOaz2o", //local
+        API_URL: 'https://cluxapi.wealwin.com',
+        FRONT_URL: 'https://clux.wealwin.com',
+        ADMIN_URL: 'https://clux.wealwin.com/admin',
+        SOCKET_URL: 'https://cluxapi.wealwin.com',
+        getGeoInfo: "https://ipapi.co/json/",
+        AUTHENTICATOR_URL: {
+            PLAY_STORE: "https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2",
+            APP_STORE: "https://apps.apple.com/us/app/google-authenticator/id388497605",
+        }
+    };
+
+} else {
+    console.log("Set Development Config")
+    // const API_URL = 'https://api.cluxchange.com/';
+    const API_URL ='http://localhost'// 'http://localhost' 'http://192.168.0.124';  //http://192.168.29.63:3000/  http://192.168.0.124
+    key = {
+        secretOrKey: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
+        CRYPTO_SECRET_KEY: "1234567812345678",
+        RECAPTCHA_SITE_KEY: "6LeHezUfAAAAAE_uuY_HFN5HoEVsQv8bpyC3xTat", //local
+        API_URL: `${API_URL}:2053`,
+        FRONT_URL: 'http://localhost', //'http://localhost:3000',
+        ADMIN_URL: 'http://localhost:3001/admin',
+        SOCKET_URL: `${API_URL}:2053`,
+        getGeoInfo: "https://ipapi.co/json/",
+        AUTHENTICATOR_URL: {
+            PLAY_STORE: "https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2",
+            APP_STORE: "https://apps.apple.com/us/app/google-authenticator/id388497605",
+        }
+    };
 }
 
 
