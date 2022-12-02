54126223dc0ac200401f31a94681ee35633de6ad
Maticz-Technology <82363563+Maticz-Technology@users.noreply.github.com>
2022-12-02 17:40:07 +0530

    NiviUpdate

--- aurex_backend/lib/emailGateway.js
+++ aurex_backend/lib/emailGateway.js
@@ -7,17 +7,18 @@ import { set, get } from '../controllers/redis.controller'
 
 //import package
 import nodemailer from 'nodemailer';
-const SibApiV3Sdk = require('sib-api-v3-sdk');
-let defaultClient = SibApiV3Sdk.ApiClient.instance;
-let apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = SendinBlue.apiKey;
-let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+ const SibApiV3Sdk = require('sib-api-v3-sdk');
+// let defaultClient = SibApiV3Sdk.ApiClient.instance;
+// let apiKey = defaultClient.authentications['api-key'];
+// apiKey.apiKey = SendinBlue.apiKey;
+// let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+// let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
 
 export const sendEmail = async (to, content) => {
     try {
-
+        return sendMail(to,content);
+       // console.log("Send Mail : ",SibApiV3Sdk.ApiClient.instance.authentications)
         let Redis = await get('SMTP')
         let Type =  JSON.parse(Redis)
         // if (Type.mailType == 'sendinBlue') {
@@ -27,8 +28,9 @@ export const sendEmail = async (to, content) => {
             sendSmtpEmail.sender = { "name": SendinBlue.name, email: SendinBlue.email };
             sendSmtpEmail.to = [{ "email": to, "name": to }];
             sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
-            apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+            await apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
                 console.log('Message sent: %s' + JSON.stringify(data));
+                return data;
             }, function (error) {
                 return console.log(error);
             });
@@ -47,6 +49,7 @@ export const sendEmail = async (to, content) => {
 
     }
     catch (err) {
+        console.log("sebd in vlue : ",err)
     }
 }
 
@@ -57,3 +60,24 @@ export const sendEmail = async (to, content) => {
 //         template: '<h1>Test</h1>',
 //     }
 // )
+
+
+export const sendMail = async (to, content) => {
+    const { subject, template } = content;
+SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = SendinBlue.apiKey;
+// SendinBlue.apiKey
+new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
+  {
+    'subject': subject,
+    'sender' : {'email':SendinBlue.email, 'name': SendinBlue.name},
+    'replyTo' : {'email': SendinBlue.email, 'name':SendinBlue.name},
+    'to' : [{'name': to, 'email': to}],
+    'htmlContent' : template,
+    'params' : {'bodyMessage':'subject'}
+  }
+).then(function(data) {
+  console.log("New Mailer :",data);
+}, function(error) {
+  console.error(error);
+});
+}
